32b0b74a7b6a338fd9aad4d605f1d7050c582184
bisratlike <bisratlike@gmail.com>
2024-03-05 16:00:25 +0300

    Lab-1-variables and types

---
+++ LAB-1-Variables-and-types.dart
@@ -0,0 +1,16 @@
+
+// exercise1
+void main() {
+  var a = 5;
+  var b = 10;
+  var c = 15;
+  print(a + b + c);
+
+// exercise 2
+String str = "welcome, I am Bisrat!";
+
+  print(str.length);
+
+}
+
+
